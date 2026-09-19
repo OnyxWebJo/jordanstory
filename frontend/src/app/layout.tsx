4f79e1d5ae92efb140d8fc160858e4bfc5a82120
OnyxWebJo <onyyx.web@gmail.com>
2026-09-19 16:26:29 +0300

    feat(analytics): install Google Tag Manager (GTM-K9G54NDB) in head and body

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "@/context/LanguageContext";
 import { CurrencyProvider } from "@/context/CurrencyContext";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,8 +21,6 @@ export const metadata: Metadata = {
   keywords: ["Jordan Story Travel & Tourism", "Jordan Story Tours", "Petra Private Tour", "Wadi Rum Desert Safari", "Dead Sea Tour", "Jordanien Reisen", "Petra Tour auf Deutsch"],
 };
 
-import Script from "next/script";
-
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
@@ -30,6 +29,21 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K9G54NDB');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
         {/* Google tag (gtag.js) */}
         <Script
           strategy="afterInteractive"
@@ -49,6 +63,17 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         </Script>
       </head>
       <body suppressHydrationWarning className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K9G54NDB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <LanguageProvider>
           <CurrencyProvider>
             {children}
